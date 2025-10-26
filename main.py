ce6d23549f6e58c5ccc3284abfcfbce4d063eff2
lagrangian-SU2 <kovalkov.kt@gmail.com>
2025-10-26 18:33:48 -0400

    added game logic

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
 import pygame
+import sys
+import player
+import asteroid
+import shot
 from constants import *
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
+    player.Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    shot.Shot.containers = (shots, drawable, updatable)
+    asteroid_field = AsteroidField()
+    player_var = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
+        for i in asteroids:
+            if i.collision(player_var):
+                print('Game Over')
+                sys.exit()
+            for j in shots:
+                if i.collision(j):
+                    j.kill()
+                    i.split()
+                    
         screen.fill('#000000')
+
+        for d in drawable:
+            d.draw(screen)
+        
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
+        
 
 
 if __name__ == "__main__":
